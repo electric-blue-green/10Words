36f6ca6e874c8c9b173b6f28c5949ac7559ef250
electric-blue-green <ThomasBass@Thomass-MBP.default>
2016-04-14 19:04:26 +0100

    added a thing

--- main.py
+++ main.py
@@ -2,27 +2,34 @@ __authors__ = ['electric-blue']
 import math
 import sys
 
-def main():
+tenLetterWords = []
+
+def main(tenLetterWords):
     word = str(input('Enter the word here: '))
-    checkWord(word)
+    checkWord(word, tenLetterWords)
 
 
-def checkWord(word):
+def checkWord(word, tenLetterWords):
     if word.isnumeric():
         print('Don\'t enter a number, silly!')
         main()
     else:
-        countWord(word)
+        countWord(word, tenLetterWords)
 
-def countWord(word):
+def countWord(word, tenLetterWords):
     if len(word) == 10:
         print(word, 'is 10 letters long!')
+        addWord(word, tenLetterWords)
     else:
         print(word, 'is not 10 letters long')
+        print('All 10 letter words:')
+        print(tenLetterWords)
     ask = input('Do another? y/n')
     if ask == 'y' or ask == 'Y':
         main()
     else:
         sys.exit()
+def addWord(word, tenLetterWords):
+    tenLetterWords.append(word)
 
-main()
+main(tenLetterWords)
